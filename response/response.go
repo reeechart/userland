b0919553fe87fe6d3a171759f2f6f0344665b5cd
Ferdinandus Richard <reeechart@gmail.com>
2019-11-11 14:21:50 +0700

    rename param for responseWithJSON

--- response/response.go
+++ response/response.go
@@ -10,11 +10,11 @@ type ErrorResponse struct {
 	Message string `json:"message"`
 }
 
-func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+func respondWithJSON(w http.ResponseWriter, statusCode int, payload interface{}) {
 	response_body, _ := json.Marshal(payload)
 
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
+	w.WriteHeader(statusCode)
 	w.Write(response_body)
 }
 
