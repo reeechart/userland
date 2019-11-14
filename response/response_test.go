afab6988793469ca94f976d29a16de2335d8768a
Ferdinandus Richard <reeechart@gmail.com>
2019-11-14 17:26:54 +0700

    add respond success with body testing

--- response/response_test.go
+++ response/response_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -19,4 +20,18 @@ func TestRespondSuccess(t *testing.T) {
 	RespondSuccess(res)
 	assert.Equal(t, http.StatusOK, res.Code)
 	assert.Equal(t, string(expectedBody), res.Body.String(), "Respond success should return success true JSON")
+}
+
+func TestRespondSuccessWithBody(t *testing.T) {
+	sampleObject := map[string]interface{}{
+		"time":    time.Now(),
+		"weather": "sunny",
+	}
+	expectedBody, err := json.Marshal(sampleObject)
+	require.Nil(t, err)
+
+	res := httptest.NewRecorder()
+	RespondSuccessWithBody(res, sampleObject)
+	assert.Equal(t, http.StatusOK, res.Code)
+	assert.Equal(t, string(expectedBody), res.Body.String())
 }
