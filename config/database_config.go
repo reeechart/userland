eeeaa825cc4de5bd21c887e37f753d9e40c97ea7
Ferdinandus Richard <reeechart@gmail.com>
2019-11-14 09:47:35 +0700

    change db config to use env vars

--- config/database_config.go
+++ config/database_config.go
@@ -2,7 +2,17 @@ package config
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
 )
+
+type databaseConfig struct {
+	dbname string
+	host   string
+	port   string
+	ssl    string
+}
 
 const (
 	DB_NAME    = "userland"
@@ -12,5 +22,22 @@ const (
 )
 
 func GetDatabaseConnectionString() string {
-	return fmt.Sprintf("dbname=%s host=%s port=%s sslmode=%s", DB_NAME, DB_HOST, DB_PORT, ENABLE_SSL)
+	dbConfig, err := getDatabaseConfig()
+	if err != nil {
+		panic(err)
+	}
+	return fmt.Sprintf("dbname=%s host=%s port=%s sslmode=%s", dbConfig.dbname, dbConfig.host, dbConfig.port, dbConfig.ssl)
+}
+
+func getDatabaseConfig() (*databaseConfig, error) {
+	err := godotenv.Load()
+	if err != nil {
+		return nil, err
+	}
+	return &databaseConfig{
+		dbname: os.Getenv("DB_NAME"),
+		host:   os.Getenv("DB_HOST"),
+		port:   os.Getenv("DB_PORT"),
+		ssl:    os.Getenv("ENABLE_SSL"),
+	}, nil
 }
