088e0675be15674cc894512491dd3ecbb6fe0eb6
Ferdinandus Richard <reeechart@gmail.com>
2019-11-15 17:06:17 +0700

    add auth handler register on valid user test

---
+++ auth/handlers_test.go
@@ -0,0 +1,100 @@
+package auth
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	res *httptest.ResponseRecorder
+
+	handler AuthHandler
+	router  *mux.Router
+
+	ctrl     *gomock.Controller
+	mockRepo *MockuserRepositoryInterface
+
+	validNewUser          userRegistration
+	invalidNewUser        userRegistration
+	incompleteNewUser     userRegistration
+	unmatchingPassNewUser userRegistration
+	validNewUserFailQuery userRegistration
+)
+
+func testAuthHandlerInit(t *testing.T) {
+	ctrl = gomock.NewController(t)
+	mockRepo = NewMockuserRepositoryInterface(ctrl)
+
+	handler = AuthHandler{UserRepo: mockRepo}
+
+	router = mux.NewRouter()
+	router.HandleFunc("/auth/register", handler.Register).Methods(http.MethodPost)
+}
+
+func testAuthHandlerEnd() {
+	ctrl.Finish()
+}
+
+func initRepoForRegistration() {
+	validNewUser = userRegistration{
+		Fullname:        "user",
+		Email:           "user@example.com",
+		Password:        "password",
+		PasswordConfirm: "password",
+	}
+
+	invalidNewUser = userRegistration{
+		Fullname:        "user",
+		Email:           "email_invalid@examplecom",
+		Password:        "password",
+		PasswordConfirm: "password",
+	}
+
+	incompleteNewUser = userRegistration{
+		Fullname: "user",
+		Email:    "user@example.com",
+	}
+
+	unmatchingPassNewUser = userRegistration{
+		Fullname:        "user",
+		Email:           "user@example.com",
+		Password:        "password",
+		PasswordConfirm: "differentpassword",
+	}
+
+	validNewUserFailQuery = userRegistration{
+		Fullname:        "user invalid query",
+		Email:           "user@example.com",
+		Password:        "password",
+		PasswordConfirm: "password",
+	}
+
+	mockRepo.EXPECT().createNewUser(validNewUser).Return(nil)
+	// mockRepo.EXPECT().createNewUser(invalidNewUser).Return(errors.New(""))
+	// mockRepo.EXPECT().createNewUser(incompleteNewUser).Return(errors.New(""))
+	// mockRepo.EXPECT().createNewUser(unmatchingPassNewUser).Return(errors.New(""))
+	// mockRepo.EXPECT().createNewUser(validNewUserFailQuery).Return(errors.New(""))
+}
+
+func TestRegister(t *testing.T) {
+	testAuthHandlerInit(t)
+	initRepoForRegistration()
+
+	userRegistrationData, err := json.Marshal(validNewUser)
+	require.Nil(t, err)
+	req, err := http.NewRequest(http.MethodPost, "/auth/register", bytes.NewReader(userRegistrationData))
+	require.Nil(t, err)
+	res = httptest.NewRecorder()
+	router.ServeHTTP(res, req)
+	assert.Equal(t, http.StatusOK, res.Code)
+
+	testAuthHandlerEnd()
+}
--- auth/user_repository_mock.go
+++ auth/user_repository_mock.go
@@ -5,8 +5,9 @@
 package auth
 
 import (
-	gomock "github.com/golang/mock/gomock"
 	reflect "reflect"
+
+	gomock "github.com/golang/mock/gomock"
 )
 
 // MockuserRepositoryInterface is a mock of userRepositoryInterface interface
