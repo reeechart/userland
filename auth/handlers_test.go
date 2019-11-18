c328e59d4f04dea0459bb829a3d9818d10df74ca
Ferdinandus Richard <reeechart@gmail.com>
2019-11-18 09:32:55 +0700

    add verification test to auth handler

--- auth/handlers_test.go
+++ auth/handlers_test.go
@@ -27,8 +27,18 @@ var (
 	unmatchingPassNewUser userRegistration
 	validNewUserFailQuery userRegistration
 
+	validVerifReq        verificationRequest
+	invalidVerifReq      verificationRequest
+	invalidTokenVerifReq verificationRequest
+	userlessVerifReq     verificationRequest
+
 	loginnableUser   User
 	unloginnableUser User
+)
+
+const (
+	SAMPLE_VALID_VERIFICATION_TOKEN   = "abcdabcdabcdabcdabcdabcdabcd"
+	SAMPLE_INVALID_VERIFICATION_TOKEN = "invalidtokeninvalidtokeninvalidt"
 )
 
 func testAuthHandlerInit(t *testing.T) {
@@ -40,6 +50,7 @@ func testAuthHandlerInit(t *testing.T) {
 	router = mux.NewRouter()
 	router.HandleFunc("/auth/register", handler.Register).Methods(http.MethodPost)
 	router.HandleFunc("/auth/login", handler.Login).Methods(http.MethodPost)
+	router.HandleFunc("/auth/verify", handler.Verify).Methods(http.MethodPost)
 }
 
 func testAuthHandlerEnd() {
@@ -103,6 +114,59 @@ func testRegisterUser(t *testing.T, newUser userRegistration, expectedStatusCode
 	userRegistrationData, err := json.Marshal(newUser)
 	require.Nil(t, err)
 	req, err := http.NewRequest(http.MethodPost, "/auth/register", bytes.NewReader(userRegistrationData))
+	require.Nil(t, err)
+	res := httptest.NewRecorder()
+	router.ServeHTTP(res, req)
+	assert.Equal(t, expectedStatusCode, res.Code)
+}
+
+func TestVerify(t *testing.T) {
+	testAuthHandlerInit(t)
+	initRepoForVerify()
+
+	testVerifyUser(t, validVerifReq, http.StatusOK)
+	testVerifyUser(t, invalidVerifReq, http.StatusBadRequest)
+	testVerifyUser(t, invalidTokenVerifReq, http.StatusBadRequest)
+	testVerifyUser(t, userlessVerifReq, http.StatusBadRequest)
+
+	testAuthHandlerEnd()
+}
+
+func initRepoForVerify() {
+	validVerifReq = verificationRequest{
+		Type:              "email.verify",
+		Recipient:         "user@example.com",
+		VerificationToken: SAMPLE_VALID_VERIFICATION_TOKEN,
+	}
+
+	invalidVerifReq = verificationRequest{
+		Type:              "email.verify",
+		VerificationToken: SAMPLE_VALID_VERIFICATION_TOKEN,
+	}
+
+	invalidTokenVerifReq = verificationRequest{
+		Type:              "email.verify",
+		Recipient:         "user@example.com",
+		VerificationToken: SAMPLE_INVALID_VERIFICATION_TOKEN,
+	}
+
+	userlessVerifReq = verificationRequest{
+		Type:              "email.verify",
+		Recipient:         "notregistered@example.com",
+		VerificationToken: SAMPLE_VALID_VERIFICATION_TOKEN,
+	}
+
+	gomock.InOrder(
+		mockRepo.EXPECT().verifyUser(validVerifReq.Recipient, validVerifReq.VerificationToken).Return(nil),
+		mockRepo.EXPECT().verifyUser(invalidTokenVerifReq.Recipient, invalidTokenVerifReq.VerificationToken).Return(errors.New("")),
+		mockRepo.EXPECT().verifyUser(userlessVerifReq.Recipient, userlessVerifReq.VerificationToken).Return(errors.New("")),
+	)
+}
+
+func testVerifyUser(t *testing.T, userVerificationRequest verificationRequest, expectedStatusCode int) {
+	verifReq, err := json.Marshal(userVerificationRequest)
+	require.Nil(t, err)
+	req, err := http.NewRequest(http.MethodPost, "/auth/verify", bytes.NewReader(verifReq))
 	require.Nil(t, err)
 	res := httptest.NewRecorder()
 	router.ServeHTTP(res, req)
