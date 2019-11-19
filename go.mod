ae23549068ab67f32318602580964abe487dba90
Ferdinandus Richard <reeechart@gmail.com>
2019-11-19 14:21:53 +0700

    change logger to use logrus

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/jmoiron/sqlx v1.2.0
 	github.com/joho/godotenv v1.3.0
 	github.com/lib/pq v1.2.0
+	github.com/sirupsen/logrus v1.4.2
 	github.com/stretchr/testify v1.4.0
 	golang.org/x/crypto v0.0.0-20191112222119-e1110fd1c708
 )
--- main.go
+++ main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
 
 	"userland/appcontext"
 	"userland/router"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
 	appcontext.InitContext()
 	router := router.GetRouter()
 
-	fmt.Println("Server is listening at 8080")
+	log.Info("Server is listening at 8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
